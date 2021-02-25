cbf35b04a041872af0196dcb6d2643a204d7fb7a
Ananev Konstantin <ananev@protei.ru>
2021-02-25 22:15:48 +0300

    [homework 1] initialisation

---
+++ homework1/src/test/java/ru/otus/AppTest.java
@@ -0,0 +1,20 @@
+package ru.otus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrueTest()
+    {
+        assertEquals( 1, 1);
+    }
+}
