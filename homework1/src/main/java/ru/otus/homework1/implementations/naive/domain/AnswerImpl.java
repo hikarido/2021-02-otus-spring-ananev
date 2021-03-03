09fc5d7cf3cd20b6ac300ee883c91f40282988bc
Ananev Konstantin <ananev@protei.ru>
2021-03-03 12:01:25 +0300

    answer impl was added

--- homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/AnswerImpl.java
+++ homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/AnswerImpl.java
@@ -3,7 +3,22 @@ package ru.otus.homework1.implementations.naive.domain;
 import ru.otus.homework1.core.domain.Answer;
 
 public class AnswerImpl implements Answer {
+
+    private String content;
+    private boolean isRight;
+
+    public AnswerImpl(String content, boolean isRight) {
+        this.content = content;
+        this.isRight = isRight;
+    }
+
+    @Override
+    public boolean isRight() {
+        return isRight;
+    }
+
+    @Override
     public String getText() {
-        return null;
+        return content;
     }
 }
