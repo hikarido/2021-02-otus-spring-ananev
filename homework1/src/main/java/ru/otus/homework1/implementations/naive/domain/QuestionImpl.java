184c234a5ec23088c4a77e24b76b1df8a7ac0a2c
Ananev Konstantin <ananev@protei.ru>
2021-03-03 12:03:03 +0300

    question impl was added

--- homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/QuestionImpl.java
+++ homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/QuestionImpl.java
@@ -3,7 +3,13 @@ package ru.otus.homework1.implementations.naive.domain;
 import ru.otus.homework1.core.domain.Question;
 
 public class QuestionImpl implements Question {
+    private String content;
+
+    public QuestionImpl(String content) {
+        this.content = content;
+    }
+
     public String getText() {
-        return null;
+        return content;
     }
 }
