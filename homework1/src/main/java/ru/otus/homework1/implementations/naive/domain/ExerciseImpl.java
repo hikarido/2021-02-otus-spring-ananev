a9d7210d15f60efa6eaff26ba93fb5b2be316721
Ananev Konstantin <ananev@protei.ru>
2021-03-03 12:02:34 +0300

    exercise impl was added

--- homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/ExerciseImpl.java
+++ homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/ExerciseImpl.java
@@ -5,17 +5,30 @@ import ru.otus.homework1.core.domain.Exercise;
 import ru.otus.homework1.core.domain.Question;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExerciseImpl implements Exercise {
+
+    private Question question;
+    private List<Answer> answers;
+
+    public ExerciseImpl(Question question, List<Answer> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
     public Question getQuestion() {
-        return null;
+        return question;
     }
 
     public List<Answer> getAnswerChoices() {
-        return null;
+        return answers;
     }
 
     public List<Answer> getAnswersOfExercise() {
-        return null;
+        return answers
+                .stream()
+                .filter(Answer::isRight)
+                .collect(Collectors.toList());
     }
 }
