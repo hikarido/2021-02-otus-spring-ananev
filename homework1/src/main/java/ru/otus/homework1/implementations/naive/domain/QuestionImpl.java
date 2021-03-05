4c4278ffad46db0f1d89668aab3433ebe1d9a867
Ananev Konstantin <ananev@protei.ru>
2021-03-05 10:27:03 +0300

    equals for question was implemented

--- homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/QuestionImpl.java
+++ homework1/src/main/java/ru/otus/homework1/implementations/naive/domain/QuestionImpl.java
@@ -2,6 +2,8 @@ package ru.otus.homework1.implementations.naive.domain;
 
 import ru.otus.homework1.core.domain.Question;
 
+import java.util.Objects;
+
 public class QuestionImpl implements Question {
     private String content;
 
@@ -11,5 +13,19 @@ public class QuestionImpl implements Question {
 
     public String getText() {
         return content;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+
+        Question o = (Question) other;
+        return Objects.equals(this.getText(), o.getText());
     }
 }
