5d06fed06f3b564b01d41ccb69352cac78863957
Ananev Konstantin <ananev@protei.ru>
2021-03-05 22:36:28 +0300

    try to run by spring. Program fails. Solving problems and smoking docs

--- homework1/src/main/java/ru/otus/Main.java
+++ homework1/src/main/java/ru/otus/Main.java
@@ -1,13 +1,12 @@
 package ru.otus;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Homework 1!" );
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.homework1.core.dao.ExerciseDao;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("/spring-context.xml");
+        ExerciseDao dao = context.getBean(ExerciseDao.class);
     }
 }
