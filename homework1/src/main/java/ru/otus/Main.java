aade01fa78f4bc49e8d49531ea9ff85ba884799f
Ananev Konstantin <ananev@protei.ru>
2021-03-06 18:40:13 +0300

    run app in idea. Works ok. But --jar does not work.
    By some reason jar can't find resources.

--- homework1/src/main/java/ru/otus/Main.java
+++ homework1/src/main/java/ru/otus/Main.java
@@ -2,11 +2,21 @@ package ru.otus;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.otus.homework1.core.dao.ExerciseDao;
+import ru.otus.homework1.core.domain.Answer;
+import ru.otus.homework1.core.domain.Exercise;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
         ExerciseDao dao = context.getBean(ExerciseDao.class);
+
+        for(Exercise e: dao.getExercises()){
+            System.out.println(e.getQuestion().getText());
+            for(Answer a: e.getAnswerChoices()){
+                System.out.println(" * " + a.getText());
+            }
+            System.out.println();
+        }
     }
 }
