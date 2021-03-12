70663661ba5e2a8ae969cacc507d8d0f2485eeee
Ananev Konstantin <ananev@protei.ru>
2021-03-12 03:05:21 +0300

    configuration was extended with new domain objects

--- homework2/src/main/java/ru/otus/homework2/config/MainConfig.java
+++ homework2/src/main/java/ru/otus/homework2/config/MainConfig.java
@@ -1,10 +1,16 @@
 package ru.otus.homework2.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import ru.otus.homework2.core.ConsoleExam;
 import ru.otus.homework2.core.ResourceAccessor;
+import ru.otus.homework2.core.dao.PersonDao;
+import ru.otus.homework2.core.domain.Exam;
+import ru.otus.homework2.implementations.naive.ConsoleExamImpl;
 import ru.otus.homework2.implementations.naive.ResourceAccessorImpl;
 import ru.otus.homework2.implementations.naive.dao.ExerciseDaoCsv;
 import ru.otus.homework2.core.dao.ExerciseDao;
+import ru.otus.homework2.implementations.naive.dao.PersonDaoImpl;
+import ru.otus.homework2.implementations.naive.domain.ExamImpl;
 
 @Configuration
 public class MainConfig {
@@ -16,5 +22,20 @@ public class MainConfig {
     @Bean
     ResourceAccessor resourceAccessor(){
         return new ResourceAccessorImpl();
+    }
+
+    @Bean
+    PersonDao personDao(){
+        return new PersonDaoImpl();
+    }
+
+    @Bean
+    Exam exam(ExerciseDao exerciseDao){
+        return new ExamImpl(exerciseDao, 5, 0);
+    }
+
+    @Bean
+    ConsoleExam consoleExam(PersonDao personDao, ExerciseDao exerciseDao, Exam exam){
+        return new ConsoleExamImpl(personDao, exerciseDao, exam);
     }
 }
