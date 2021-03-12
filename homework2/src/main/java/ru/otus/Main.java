64848193391a81ef8a44450a65f923fe8c34184b
Ananev Konstantin <ananev@protei.ru>
2021-03-12 03:05:35 +0300

    main runs exam

--- homework2/src/main/java/ru/otus/Main.java
+++ homework2/src/main/java/ru/otus/Main.java
@@ -3,6 +3,7 @@ package ru.otus;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.otus.homework2.config.MainConfig;
+import ru.otus.homework2.core.ConsoleExam;
 import ru.otus.homework2.core.dao.ExerciseDao;
 import ru.otus.homework2.core.domain.Answer;
 import ru.otus.homework2.core.domain.Exercise;
@@ -11,14 +12,8 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(MainConfig.class);
-        ExerciseDao dao = context.getBean(ExerciseDao.class);
 
-        for(Exercise e: dao.getExercises()){
-            System.out.println(e.getQuestion().getText());
-            for(Answer a: e.getAnswerChoices()){
-                System.out.println(" * " + a.getText());
-            }
-            System.out.println();
-        }
+        ConsoleExam consoleExam = context.getBean(ConsoleExam.class);
+        consoleExam.performExam();
     }
 }
