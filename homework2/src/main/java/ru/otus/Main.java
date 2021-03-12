9954fd426a372e1de53d6bdf634770c0e65027e4
Ananev Konstantin <ananev@protei.ru>
2021-03-13 00:35:16 +0300

    dead code deletion

--- homework2/src/main/java/ru/otus/Main.java
+++ homework2/src/main/java/ru/otus/Main.java
@@ -4,9 +4,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.otus.homework2.config.MainConfig;
 import ru.otus.homework2.core.ConsoleExam;
-import ru.otus.homework2.core.dao.ExerciseDao;
-import ru.otus.homework2.core.domain.Answer;
-import ru.otus.homework2.core.domain.Exercise;
 
 public class Main {
     public static void main(String[] args) {
