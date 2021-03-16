c1ded10a31b5285bd578a8b4802fbfe7f55b0753
Ananev Konstantin <ananev@protei.ru>
2021-03-16 23:59:12 +0300

    app settings configuration was added

---
+++ homework3/src/main/java/ru/otus/homework3/config/AppSettings.java
@@ -0,0 +1,10 @@
+package ru.otus.homework3.config;
+
+import java.util.Locale;
+
+public interface AppSettings {
+    /**
+     * @return locale which should be used in whole app
+     */
+    Locale getAppLocale();
+}
