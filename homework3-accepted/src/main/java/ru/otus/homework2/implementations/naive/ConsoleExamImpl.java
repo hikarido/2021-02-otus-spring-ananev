a7e6cfa37668c21e66271a7908a1129695cadaeb
Ananev Konstantin <ananev@protei.ru>
2021-03-22 22:15:55 +0300

    hw3 added and readme was extended

---
+++ homework3-accepted/src/main/java/ru/otus/homework2/implementations/naive/ConsoleExamImpl.java
@@ -0,0 +1,89 @@
+package ru.otus.homework2.implementations.naive;
+
+import org.springframework.context.MessageSource;
+import ru.otus.homework2.core.ConsoleExam;
+import ru.otus.homework2.core.dao.ExerciseDao;
+import ru.otus.homework2.core.dao.PersonDao;
+import ru.otus.homework2.core.domain.Answer;
+import ru.otus.homework2.core.domain.Exam;
+import ru.otus.homework2.core.domain.Exercise;
+import ru.otus.homework2.core.domain.Person;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsoleExamImpl implements ConsoleExam {
+    private final PersonDao personDao;
+    private final ExerciseDao exerciseDao;
+    private final Exam exam;
+    private Scanner scanner = new Scanner(System.in);
+    private MessageSource messageSource;
+    private Locale locale;
+
+    public ConsoleExamImpl(
+            PersonDao personDao,
+            ExerciseDao exerciseDao,
+            Exam exam,
+            MessageSource messageSource,
+            Locale locale) {
+        this.personDao = personDao;
+        this.exerciseDao = exerciseDao;
+        this.exam = exam;
+        this.messageSource = messageSource;
+        this.locale = locale;
+    }
+
+    @Override
+    public void performExam() {
+        Person student = getStudent();
+        writeMessageToConsole(messageSource.getMessage("console.welcome", new String[]{student.toString()}, locale));
+        for (Exercise exercise : exam.getExercises()) {
+            writeMessageToConsole(exercise.getQuestion().getText());
+            Map<Integer, Answer> sequenceNumberToAnswer = new HashMap<>();
+            int number = 0;
+            for (Answer answer : exercise.getAnswerChoices()) {
+                sequenceNumberToAnswer.put(number, answer);
+                writeMessageToConsole(number + ") " + answer.getText());
+                number += 1;
+            }
+            List<Answer> answers = getUserAnswers(sequenceNumberToAnswer);
+            exam.answer(exercise, answers);
+        }
+
+        writeMessageToConsole(messageSource.getMessage(
+                "console.test-outcome",
+                new String[]{student.toString(), String.valueOf(exam.wasExamPassed())},
+                locale
+        ));
+    }
+
+    private Person getStudent() {
+        writeMessageToConsole(messageSource.getMessage("console.enter-user-name", null, locale));
+        String firstName = getUserInput();
+        writeMessageToConsole(messageSource.getMessage("console.enter-user-second-name", null, locale));
+        String secondName = getUserInput();
+        return personDao.addPerson(firstName, secondName);
+    }
+
+    private void writeMessageToConsole(String msg) {
+        System.out.println(msg);
+    }
+
+    private String getUserInput() {
+        writeMessageToConsole(">>> ");
+        return scanner.nextLine();
+    }
+
+    private List<Answer> getUserAnswers(Map<Integer, Answer> sequenceNumberToAnswer) {
+        writeMessageToConsole(messageSource.getMessage("console.note-about-plural-answer", null, locale));
+        String input = getUserInput();
+        List<Integer> indexes =
+                Arrays.stream(input.split(" ")).map(Integer::valueOf).collect(Collectors.toList());
+        List<Answer> answers = new ArrayList<>();
+        for (Integer i : indexes) {
+            answers.add(sequenceNumberToAnswer.get(i));
+        }
+
+        return answers;
+    }
+}
---
+++ homework3-accepted/src/test/java/ru/otus/homework2/implementations/naive/domain/ExerciseImplTest.java
@@ -0,0 +1,51 @@
+package ru.otus.homework2.implementations.naive.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.otus.homework2.core.domain.Answer;
+import ru.otus.homework2.core.domain.Exercise;
+import ru.otus.homework2.core.domain.Question;
+
+import java.util.List;
+
+class ExerciseImplTest {
+    @Test
+    void createTest() {
+        Answer answer = Mockito.mock(Answer.class);
+        Question question = Mockito.mock(Question.class);
+        new ExerciseImpl(question, List.of(answer));
+    }
+
+    @Test
+    void getQuestion(){
+        Answer answer = Mockito.mock(Answer.class);
+        Question question = Mockito.mock(Question.class);
+        Exercise exercise = new ExerciseImpl(question, List.of(answer));
+        Assertions.assertEquals(question, exercise.getQuestion());
+    }
+
+    @Test
+    void getAnswerChoices(){
+        Answer rightAnswer = Mockito.mock(Answer.class);
+        Answer wrongAnswer = Mockito.mock(Answer.class);
+        Mockito.when(rightAnswer.isRight()).thenReturn(true);
+        Mockito.when(wrongAnswer.isRight()).thenReturn(false);
+
+        Question question = Mockito.mock(Question.class);
+        Exercise exercise = new ExerciseImpl(question, List.of(rightAnswer, wrongAnswer));
+        Assertions.assertEquals(List.of(rightAnswer, wrongAnswer), exercise.getAnswerChoices());
+    }
+
+    @Test
+    void getAnswerOfExercise(){
+        Answer rightAnswer = Mockito.mock(Answer.class);
+        Answer wrongAnswer = Mockito.mock(Answer.class);
+        Mockito.when(rightAnswer.isRight()).thenReturn(true);
+        Mockito.when(wrongAnswer.isRight()).thenReturn(false);
+
+        Question question = Mockito.mock(Question.class);
+        Exercise exercise = new ExerciseImpl(question, List.of(rightAnswer, wrongAnswer));
+        Assertions.assertEquals(List.of(rightAnswer), exercise.getAnswersOfExercise());
+    }
+}
