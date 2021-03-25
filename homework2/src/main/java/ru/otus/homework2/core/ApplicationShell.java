25d3745ff9c9f0924fc67cddcf73764f5e87f5d4
Ananev Konstantin <ananev@protei.ru>
2021-03-25 03:47:28 +0300

    [homework4] localisation was used

--- homework2/src/main/java/ru/otus/homework2/core/ApplicationShell.java
+++ homework2/src/main/java/ru/otus/homework2/core/ApplicationShell.java
@@ -3,14 +3,17 @@ package ru.otus.homework2.core;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import ru.otus.homework2.i18n.LocalisationService;
 
 @ShellComponent
 public class ApplicationShell {
 
     private ConsoleExam consoleExam;
+    private LocalisationService localisationService;
 
-    public ApplicationShell(ConsoleExam consoleExam) {
+    public ApplicationShell(ConsoleExam consoleExam, LocalisationService localisationService) {
         this.consoleExam = consoleExam;
+        this.localisationService = localisationService;
     }
 
     @ShellMethod(value = "Only logged users can take exam. It do this stuff.", key = {"l", "login"})
@@ -18,24 +21,29 @@ public class ApplicationShell {
             @ShellOption({"-fn", "--firstName"}) String firstName,
             @ShellOption({"-sn", "--secondName"}) String secondName) {
         consoleExam.login(firstName, secondName);
-        return frameByNewLineSymbolAroundText("Hello " + consoleExam.getCurrentUserName());
+        return frameByNewLineSymbolAroundText(localisationService.localize("console.welcome", consoleExam.getCurrentUserName()));
     }
 
     @ShellMethod(value = "Get next question of exam", key = {"n", "nextQuestion"})
     public String nextQuestion() {
         if (!consoleExam.wasUserLogged()) {
-            return frameByNewLineSymbolAroundText("User have not been logged yet!");
+            return frameByNewLineSymbolAroundText(localisationService.localize("console.no-login-yet"));
         }
 
         if (!consoleExam.currentAnswerWasTaken()) {
-            return frameByNewLineSymbolAroundText("First answer to previous question. Then try get next.");
+            return frameByNewLineSymbolAroundText(localisationService.localize("console.unanswered-question-exists"));
         }
 
         if (consoleExam.hasNextExercise()) {
             return frameByNewLineSymbolAroundText(consoleExam.nextExerciseText());
         } else {
-            String wasPassed = consoleExam.wasExamPassed() ? "passed" : "not passed";
-            return frameByNewLineSymbolAroundText("Exam already done. You " + wasPassed + " exam");
+            String wasPassed = consoleExam.wasExamPassed() ?
+                    localisationService.localize("console.pass-exam") :
+                    localisationService.localize("console.nopass-exam");
+            return frameByNewLineSymbolAroundText(localisationService.localize(
+                    "console.end-of-exam-result",
+                    wasPassed)
+            );
         }
     }
 
@@ -45,15 +53,15 @@ public class ApplicationShell {
     )
     public String answer(String answerText) {
         if (!consoleExam.wasUserLogged()) {
-            return frameByNewLineSymbolAroundText("User have not been logged yet!");
+            return frameByNewLineSymbolAroundText(localisationService.localize("console.no-login-yet"));
         }
 
         if (consoleExam.currentAnswerWasTaken()) {
-            return frameByNewLineSymbolAroundText("There is no question to answer");
+            return frameByNewLineSymbolAroundText(localisationService.localize("console.no-questions-yet"));
         }
 
         consoleExam.setAnswerOfUser(answerText);
-        return frameByNewLineSymbolAroundText("ok");
+    return frameByNewLineSymbolAroundText(localisationService.localize("console.user-input-was-accepted"));
     }
 
     private String frameByNewLineSymbolAroundText(String text) {
