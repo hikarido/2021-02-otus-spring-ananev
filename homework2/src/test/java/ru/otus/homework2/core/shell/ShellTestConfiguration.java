11eaeeeb09fe959dda5fc8b7675eb5716332a580
Ananev Konstantin <ananev@protei.ru>
2021-03-25 03:18:00 +0300

    [homework4] test for shell was added

---
+++ homework2/src/test/java/ru/otus/homework2/core/shell/ShellTestConfiguration.java
@@ -0,0 +1,13 @@
+package ru.otus.homework2.core.shell;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.Shell;
+import ru.otus.homework2.core.ApplicationShell;
+import ru.otus.homework2.core.ConsoleExam;
+
+@Configuration
+public class ShellTestConfiguration {
+
+}
