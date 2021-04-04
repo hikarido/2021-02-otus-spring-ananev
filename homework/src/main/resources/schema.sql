ce132fadfbbd3431d9ff4bfec2493b6ebe9421f0
Ananev Konstantin <ananev@protei.ru>
2021-04-04 17:24:01 +0300

    drop constraints and change id type to long

--- homework/src/main/resources/schema.sql
+++ homework/src/main/resources/schema.sql
@@ -1,21 +1,19 @@
 drop table if exists author cascade;
 create table author(
-    id int primary key auto_increment,
+    id long primary key auto_increment,
     full_name varchar(255) not null
 );
 
 drop table if exists genre cascade;
 create table genre(
-    id int primary key auto_increment,
+    id long primary key auto_increment,
     name varchar(255) not null
 );
 
 drop table if exists book cascade;
 create table book(
-    id int primary key auto_increment,
-    author_id int,
-    genre_id int,
-    title varchar(255),
-    constraint book_genre_fk foreign key (genre_id) references genre (id),
-    constraint book_author_fk foreign key (author_id) references author (id)
+    id long primary key auto_increment,
+    author_id long,
+    genre_id long,
+    title varchar(255)
 );
